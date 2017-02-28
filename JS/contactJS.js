643d58a27f4ae46735ec52764a9abff6e7edf820
sbanish <sjb111@pitt.edu>
2017-02-28 14:00:48 -0500

    trying to finish project

---
+++ JS/contactJS.js
@@ -0,0 +1,44 @@
+function submitFunction()
+{
+	confirm("Are you sure you want to submit this information?");
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
+	
+	if (name=="" && email=="" && message=="")
+	{
+		alert("You have left name, email, and message blank. Please enter a value.");
+	}
+	else if(name=="" && email=="")
+	{
+		alert("You left the name and email fields empty.");
+	}
+	else if(name=="" && message=="")
+	{
+		alert("You left the name and message fields empty.");
+	}
+	else if(message=="" && email=="")
+	{
+		alert("You left the email and message fields empty.");
+	}
+	else if(name=="" && message=="")
+	{
+		alert("You left the name and message fields empty.");
+	}
+	else if(name=="")
+	{
+		alert("You left the name field empty.");
+	}
+	else if (email=="")
+	{
+		alert("You left the email field empty.");
+	}
+	else if (message=="")
+	{
+		alert("You left the message field empty.");
+	}
+	else
+	{
+		alert("You have successfully submitted your contact information!");
+	}
+}
